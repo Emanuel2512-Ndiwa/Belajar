5095f367772051277368b84399f136a30c9214de
EMAN NDIWA <emanndiwa134@gmail.com>
2026-03-29 11:21:46 +0700

    Belajar Percabangan

---
+++ Belajar_java/Comparasion.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Comparasion {
+    public static void main(String[] args) {
+        // Membuat objek Scanner untuk membaca input dari pengguna
+        Scanner input = new Scanner(System.in);
+
+        int number1, number2;
+
+        // Meminta pengguna untuk memasukkan dua angka
+        System.out.print("Masukkan angka pertama: ");
+        number1 = input.nextInt();
+        System.out.print("Masukkan angka kedua: ");
+        number2 = input.nextInt();
+        // Membandingkan kedua angka dan menampilkan hasilnya
+
+        if(number1 == number2){
+            System.out.printf("%d == %d%n", number1, number2);
+
+        }
+
+        if(number1 != number2){
+            System.out.printf("%d != %d%n", number1, number2);
+
+        }
+
+        if(number1 < number2){
+            System.out.printf("%d < %d%n", number1, number2);
+
+        }
+        if(number1 > number2){
+            System.out.printf("%d > %d%n", number1, number2);
+
+        }
+        if(number1 <= number2){
+            System.out.printf("%d <= %d%n", number1, number2);
+
+        }
+        if(number1 >= number2){
+            System.out.printf("%d >= %d%n", number1, number2);
+
+        }
+    }
+
+}
